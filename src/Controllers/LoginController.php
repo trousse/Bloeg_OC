a9b995fbaaeccd41c7b7ef2682fa093dcf012ad0
trousse <ths.rousse@gmail.com>
2021-10-29 12:37:00 +0200

    mise ne place

--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -11,6 +11,7 @@ namespace APP\Blog\controllers;
             switch ($page){
                 case 'subscribe':
                     $this->subscribe();
+                    break;
             }
         }
 
